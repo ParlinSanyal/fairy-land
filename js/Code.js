63e925f756b20d8c892dff4e90108b377efd0c60
ParlinSanyal <87417662+ParlinSanyal@users.noreply.github.com>
2023-01-26 11:35:55 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "PIXIEDUST";
+const accessCode2 = "FAIRYLAND";
+const accessCode3 = "TINKERBELL";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text(" D P X U I S E I T", 100,50)
+    fill("lightblue")
+    text("Hint: Fairy needs it to fly !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("F L I A N R D A Y", 700,150)
+    fill("lightblue")
+    text("Hint: Fairy's live here !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text(" B T L N L E I E K R", 100,250)
+    fill("lightblue")
+    text("Hint: Fairy,s name who sprinkles pixie dust around !!", 100,270)
+
+}
